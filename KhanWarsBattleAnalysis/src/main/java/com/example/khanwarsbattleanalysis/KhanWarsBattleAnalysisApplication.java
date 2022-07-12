ad19a5bac844b99fdc010ba12143e7723f651b7d
Panchev <neno.panchev@gmail.com>
2022-07-12 23:02:40 +0300

    KhanWars app started

---
+++ KhanWarsBattleAnalysis/src/main/java/com/example/khanwarsbattleanalysis/KhanWarsBattleAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.khanwarsbattleanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanWarsBattleAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhanWarsBattleAnalysisApplication.class, args);
+    }
+
+}
