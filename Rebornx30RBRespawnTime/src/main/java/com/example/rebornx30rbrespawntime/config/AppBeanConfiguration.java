91e9404213e2dac138c73bed0ea905012055da4d
Panchev <neno.panchev@gmail.com>
2022-08-14 19:54:41 +0300

    after finding json, removed webdriver and replaced audioService with HTML <audio>

--- Rebornx30RBRespawnTime/src/main/java/com/example/rebornx30rbrespawntime/config/AppBeanConfiguration.java
+++ Rebornx30RBRespawnTime/src/main/java/com/example/rebornx30rbrespawntime/config/AppBeanConfiguration.java
@@ -6,10 +6,8 @@ import org.modelmapper.ModelMapper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 public class AppBeanConfiguration {
     @Bean
     public Gson gson() {
